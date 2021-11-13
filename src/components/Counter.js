9440d759b96ed852beb437fc3526532a33603303
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-13 15:34:52 -0300

    Dispatching Actions From Inside Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { COUNTER_INCREMENT, COUNTER_DECREMENT } from '../store';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({
+      type: COUNTER_INCREMENT,
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: COUNTER_DECREMENT,
+    });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -11,6 +25,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 
-const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
-const COUNTER_DECREMENT = 'COUNTER_DECREMENT';
+export const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
+export const COUNTER_DECREMENT = 'COUNTER_DECREMENT';
 
 const initialState = { counter: 0 };
 
