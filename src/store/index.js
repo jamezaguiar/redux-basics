0633973a50adf35353fb9e681e46bde63f0e2a16
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-14 08:10:49 -0300

    Adding State Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
 export const COUNTER_DECREMENT = 'COUNTER_DECREMENT';
@@ -6,6 +7,25 @@ export const COUNTER_INCREASE = 'COUNTER_INCREASE';
 export const COUNTER_TOGGLE = 'COUNTER_TOGGLE';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
